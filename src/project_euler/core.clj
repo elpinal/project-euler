bb1d8728863fc0cdd4c5515c56d203552aa49a4d
elpinal <elpinal@users.noreply.github.com>
2017-06-18 17:19:34 +0900

    Done problem 1

--- src/project_euler/core.clj
+++ src/project_euler/core.clj
@@ -1,6 +1,12 @@
 (ns project-euler.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn is3or5 [n]
+  (let [i3 (mod n 3)
+        i5 (mod n 5)]
+   (or (= 0 i3) (= 0 i5))
+   ))
+
+(defn problem1
+  "Problem 1"
+  []
+  (apply + (filter is3or5 (range 1 1000))))
